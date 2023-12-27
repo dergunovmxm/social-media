a8d13d6b0e7e839c52fb06ff17f9dbe93126febd
Derga <derguno402@gmail.com>
2023-12-27 11:25:03 +0400

    описаны запросы

--- src/$api/api.fetch.ts
+++ src/$api/api.fetch.ts
@@ -1,47 +1,52 @@
 class FetchClient {
 
-	private url: string;
-	private defaultHeader: Record<string, string>
 	private API_URL = process.env.API_URL as string
 
 
-	constructor(url: string, defaultHeader: Record<string, string> = {}) {
-		this.url = this.API_URL + url
-		this.defaultHeader = defaultHeader
-	}
+	constructor(private defaultHeader: Record<string, string> = {}) {}
 
-	async query<T>(
-		query?: string,
-		variables?: Record<string, any>,
+	async get<T>(
+		path: string,
 		headers?: Record<string, string>,
 	): Promise<T> {
-		return this.fetch({ query, variables }, headers)
+		return this.fetch( path, 'GET', undefined, headers)
 	}
 
-	private async fetch(
+	async post<T>(
+		path: string,
+		body?: Record<string, any>,
+		headers?: Record<string, string>
+	): Promise<T> {
+		return this.fetch( path, 'POST', body, headers)
+	}
+
+	private async fetch<T>(
+		path: string,
+		method: string,
 		body: Record<string, any>,
 		headers?: Record<string, string>,
-	): Promise<any> {
+	): Promise<T> {
+		const url = `${this.API_URL}/${path}`
 		try {
 			const response = await fetch(
-				this.url,
+				url,
 				{
-					method: 'POST',
+					method,
 					headers: {
 						'Content-Type': 'application/json',
 						...this.defaultHeader,
 						...headers
-					},
-					body: JSON.stringify(body)
+					}, 
+					body: body ? JSON.stringify(body) : null
 				})
-			
+
 				const data = await response.json()
 
-				if (data.errors) {
-					console.log('Fetch error',data.errors)
-					throw new Error('Fetch error: ' + JSON.stringify(data.errors))
+				if (!response.ok) {
+					console.log('Fetch error',data)
+					throw new Error('Fetch error: ' + JSON.stringify(data))
 				} else {
-					return data.data
+					return data
 				}
 		} catch(error){
 			console.log('Fetch error',error)
